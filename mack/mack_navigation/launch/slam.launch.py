a16eb08f50a024478c3cd7dbc05177fbdb17db26
Makarand-Mangaonkar <makarand.ericrobotics@gmail.com>
2025-02-04 16:57:35 +0530

    Added navigation

---
+++ mack/mack_navigation/launch/slam.launch.py
@@ -0,0 +1,90 @@
+
+import os
+from launch import LaunchDescription
+from launch import LaunchContext
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import EnvironmentVariable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    slam_launch_path = PathJoinSubstitution(
+        [FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py']
+    )
+
+    slam_config_path = PathJoinSubstitution(
+        [FindPackageShare('mack_navigation'), 'config', 'slam.yaml']
+    )
+
+    navigation_launch_path = PathJoinSubstitution(
+        [FindPackageShare('nav2_bringup'), 'launch', 'navigation_launch.py']
+    )
+
+    nav2_config_path = PathJoinSubstitution(
+        [FindPackageShare('mack_navigation'), 'config', 'navigation.yaml']    
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('mack_navigation'), 'rviz', 'mack_slam.rviz']
+    )
+
+    default_map_path = PathJoinSubstitution(
+        [FindPackageShare('mack_navigation'), 'maps', 'eric.yaml']
+    )
+    
+    lc = LaunchContext()
+    ros_distro = EnvironmentVariable('ROS_DISTRO')
+    slam_param_name = 'slam_params_file'
+    if ros_distro.perform(lc) == 'humble': 
+        slam_param_name = 'params_file'
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='sim', 
+            default_value='false',
+            description='Enable use_sime_time to true'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='true',
+            description='Run rviz'
+        ),
+
+        DeclareLaunchArgument(
+            name='map', 
+            default_value=default_map_path,
+            description='Navigation map path'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(navigation_launch_path),
+            launch_arguments={
+                'map': LaunchConfiguration("map"),
+                'use_sim_time': LaunchConfiguration("sim"),
+                'params_file': nav2_config_path
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch_path),
+            launch_arguments={
+                'use_sim_time': LaunchConfiguration("sim"),
+                slam_param_name: slam_config_path
+            }.items()
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{'use_sim_time': LaunchConfiguration("sim")}]
+        )
+    ])
